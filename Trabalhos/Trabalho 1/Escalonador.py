4c4097357c005b2df51c7f82720a52611c861eb3
belther7 <lucasbeccon@gmail.com>
2019-04-29 21:45:38 -0300

    Ajuste no nome das variáveis
    Falta incluir time slice
    se possível, tentar incluir menu de escolha

--- Trabalhos/Trabalho 1/Escalonador.py
+++ Trabalhos/Trabalho 1/Escalonador.py
@@ -1,50 +1,50 @@
 #Escalonador
 
 def findWaitingTime(processes, n, 
-                    bt, wt): #tempo para todos os processos
+                    bursttime, waitingtime): #tempo para todos os processos
       
-    wt[0] = 0 #tempo do primeiro processo é 0
+    waitingtime[0] = 0 #tempo de espera do primeiro processo é 0
     
     for i in range(1, n ): 
-        wt[i] = bt[i - 1] + wt[i - 1]  #calculando tempo de espera
+        waitingtime[i] = bursttime[i - 1] + waitingtime[i - 1]  #calculando tempo de espera
   
 def findTurnAroundTime(processes, n,  
-                       bt, wt, tat): #função para troca
+                       bursttime, waitingtime, tat): #função para troca de processo
     
     for i in range(n): 
-        tat[i] = bt[i] + wt[i] # calculando tempo de troca com bt[i] + wt[i] 
+        tat[i] = bursttime[i] + waitingtime[i] # calculando tempo de troca com bursttime[i] + waitingtime[i] 
 
-def findavgTime( processes, n, bt): #Função para calcular tempo médio
+def findavgTime( processes, n, bursttime): #Função para calcular tempo médio
   
-    wt = [0] * n 
+    waitingtime = [0] * n 
     tat = [0] * n  
-    total_wt = 0
+    totalwait = 0
     total_tat = 0
     
-    findWaitingTime(processes, n, bt, wt) #Função apra calcular tempo de espera de todos os processos 
+    findWaitingTime(processes, n, bursttime, waitingtime) #Função apra calcular tempo de espera de todos os processos 
     
     findTurnAroundTime(processes, n,  
-                       bt, wt, tat) #Função para localizar tempo de troca em todos os processos
+                       bursttime, waitingtime, tat) #Função para localizar tempo de troca em todos os processos
     
     print( "Tempo de burst " + 
                   " Tempo de espera " + 
                 " Tempo de troca") #Mostrar processos com todos os detalhes
     
     for i in range(n): #Calcula tempo total de espera e tempo total de troca
-        total_wt = total_wt + wt[i] 
+        totalwait = totalwait + waitingtime[i] 
         total_tat = total_tat + tat[i] 
         print(" " + str(i + 1) + "\t\t" + 
-                    str(bt[i]) + "\t " + 
-                    str(wt[i]) + "\t\t " + 
+                    str(bursttime[i]) + "\t " + 
+                    str(waitingtime[i]) + "\t\t " + 
                     str(tat[i]))  
   
     print( "Tempo médio de espera = "+
-                   str(total_wt / n)) 
+                   str(totalwait / n)) 
     print("Tempo médio de troca = "+
                      str(total_tat / n)) 
   
 if __name__ =="__main__": 
-    processes = [ 1, 2, 3, 4, 5, 6, 7, 8] #ID de processos
+    processes = [ 1, 2, 3, 4, 5, 6, 7, 8] #ID de cada processo
     n = len(processes)
 
     burst_time = [12, 5, 8, 5, 17, 4, 1, 15] #Tempo de burst de todos os processos
